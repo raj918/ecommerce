e79300b243d6d75e90d062dea0ce30a42a087edb
vaibhav <rajchaturvedi918@gmail.com>
2025-03-01 13:31:55 -0800

    git

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,7 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -8,4 +11,20 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Merge default Metro config with custom config
+const mergedConfig = mergeConfig(getDefaultConfig(__dirname), config);
+
+// Wrap with Reanimated Metro Config
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
+
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import HomeScreen from './HomeScreen'
+
+
+
+const Drawer = createDrawerNavigator();
 
 const Main = () => {
   return (
-    <View>
-      <Text>Maifn</Text>
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen
+       name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }} />
+      {/* <Drawer.Screen name="Settings" component={Settings} /> */}
+    </Drawer.Navigator>
+   
+   
   )
 }
 
